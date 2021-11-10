e0c35ac93161fb7d73d3e9234748bbf3fa3f13c2
dnwest <cristian.daniel.fernandes@gmail.com>
2021-11-09 22:37:15 -0300

    Entrega Checkpoint Programação-Imperativa

---
+++ CristianFernandes/CTD/Programacao-imperativa/Checkpoint I/checkPointI.js
@@ -0,0 +1,65 @@
+
+function pipoca(temp){
+
+if(temp >= 21 && temp < 30){
+    console.log("Comida queimou!")
+}else if(temp < 10){
+    console.log("Tempo insuficiente!")
+}else if(temp > 30){
+    console.log("kabumm")
+}else
+console.log("Prato pronto, bom apetite!!!")
+}
+
+function macarrao(temp){
+
+    if(temp >= 16 && temp < 24){
+        console.log("Comida queimou!")
+    }else if(temp < 8){
+        console.log("Tempo insuficiente!")
+    }else if(temp > 24){
+        console.log("kabumm")
+    }else
+    console.log("Prato pronto, bom apetite!!!")
+    }
+
+    function carne(temp){
+
+        if(temp >= 31 && temp < 45){
+            console.log("Comida queimou!")
+        }else if(temp < 15){
+            console.log("Tempo insuficiente!")
+        }else if(temp > 45){
+            console.log("kabumm")
+        }else
+        console.log("Prato pronto, bom apetite!!!")
+        }
+
+        function feijao(temp){
+
+            if(temp >= 25 && temp < 35){
+                console.log("Comida queimou!")
+            }else if(temp < 12){
+                console.log("Tempo insuficiente!")
+            }else if(temp > 36){
+                console.log("kabumm")
+            }else
+            console.log("Prato pronto, bom apetite!!!")
+            }
+
+          function brigadeiro(temp){
+
+                if(temp >= 17 && temp < 24){
+                    console.log("Comida queimou!")
+                }else if(temp < 8){
+                    console.log("Tempo insuficiente!")
+                }else if(temp > 24){
+                    console.log("kabumm")
+                }else
+                console.log("Prato pronto, bom apetite!!!")
+                }       
+pipoca(9)
+macarrao(20)
+carne(18)
+feijao(60)
+brigadeiro(10)
