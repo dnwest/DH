630166ee763fa76f59549c2e7e65aa738f7c21a4
dnwest <cristian.daniel.fernandes@gmail.com>
2021-11-22 21:16:23 -0300

    Aula e mesa 13 Front

--- CristianFernandes/CTD/Programacao-imperativa/Aula12/mesa12.js
+++ CristianFernandes/CTD/Programacao-imperativa/Aula12/mesa12.js
@@ -1,6 +1,6 @@
-// 1 -
+// 1 - Descobrir o ganhador
 
-const alicia = [23, 69, 43];
+const alicia = [23, 70, 43];
 const bob = [23, 69, 43];
 encontrarGanhador(alicia, bob);
 function encontrarGanhador (a, b) {
@@ -22,4 +22,20 @@ if(pontosPrimeiroParticipante > pontosSegundoParticipante){
 }else{
     console.log("Deu impate, ambos não marcaram nenhum ponto!");
 }
+}
+
+// 2 - Bônus Extra
+
+let par = 0;
+let impar = 0;
+digitalHouse(par, impar);
+function digitalHouse(a, b){
+    a = 1;
+    b = 2;
+    for (let i = 0; i < a.length; i++){
+        if (a+i < 99){
+impar = impar + 1;
+        }
+    }
+    console.log(impar);
 }
