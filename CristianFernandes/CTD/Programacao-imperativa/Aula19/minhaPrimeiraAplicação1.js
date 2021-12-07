6e8a2c06c3fc4b88c17f604246b38a11b97b312d
Cristian Fernandes <62063380+dnwest@users.noreply.github.com>
2021-12-07 17:19:35 -0300

    Finalizado mesa aula 19

--- CristianFernandes/CTD/Programacao-imperativa/Aula19/minhaPrimeiraAplicação1.js
+++ CristianFernandes/CTD/Programacao-imperativa/Aula19/minhaPrimeiraAplicação1.js
@@ -6,10 +6,13 @@ data.forEach(function (a) {
     maiorAlt = Math.max(a.altura, maiorAlt);
 });
 
-let menorAlt = Math.min.apply(Math, data.map(function (b) { 
-    return b.altura; 
+let menorAlt = Math.min.apply(Math, data.map(function (b) {
+    return b.altura;
 }));
 
-console.log('A maior altura é ' + maiorAlt + '\n' + 'E a menor altura é ' + menorAlt);
+const contador = function (qtd) {
+    cont = qtd.filter((i) => i.sexo == "M");
+    return cont.length;
+};
 
-
+console.log('A maior altura é ' + maiorAlt + '\n' + 'E a menor altura é ' + menorAlt + '\n' + 'A quanditade de homens é de: ' + contador(data));
