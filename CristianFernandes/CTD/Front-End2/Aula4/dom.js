134caf07aa7f897611409306b052a031644b0639
Cristian Fernandes <cristian.daniel.fernandes@gmail.com>
2022-03-02 17:11:19 -0300

    Manipulando DOM

---
+++ CristianFernandes/CTD/Front-End2/Aula4/dom.js
@@ -0,0 +1,14 @@
+//const html = document.querySelector('html');
+//console.log(html)
+
+//const head = document.querySelector('head');
+//console.log(head)
+
+//const body = document.querySelector('body');
+//console.log(body)
+
+//const div = document.querySelector('div');
+//console.log(div)
+
+const section = document.querySelector('section');
+console.log(section)
