ae9aa60a038a1bcae56b1ea6e3a399522146e2e0
dnwest <cristian.daniel.fernandes@gmail.com>
2022-03-08 21:14:16 -0300

    Aula6

---
+++ CristianFernandes/CTD/Front-End2/Aula6/darkMode.js
@@ -0,0 +1,21 @@
+function dark() {
+  let element = document.querySelector('body');
+  element.classList.toggle('dark-mode');
+
+  let title = document.querySelector('h1');
+  title.classList.toggle('h1Dark');
+
+  const items = document.querySelectorAll(".item");
+  items.forEach(item => {
+    item.classList.toggle("itemDark");
+  })
+
+  const documentText = document.querySelectorAll('h2 , p');
+  documentText.forEach(i => {
+    i.classList.toggle('textDark');
+  })
+  
+//Estilização do Botão 
+  let btn = document.querySelector('.btnDark');
+  btn.classList.toggle('.btnLight');
+}
