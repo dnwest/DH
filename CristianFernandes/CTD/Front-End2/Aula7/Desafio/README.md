b037d9f575cda2a7e7f7290190ac485d2a216d8a
dnwest <cristian.daniel.fernandes@gmail.com>
2022-03-09 21:34:44 -0300

    Aula7

---
+++ CristianFernandes/CTD/Front-End2/Aula7/Desafio/README.md
@@ -0,0 +1,8 @@
+# DH
+# Mesa de trabalho - Equipe 4
+# Cristian Fernandes
+# Douglas Da Silva Nascimento
+# Kened Silva Felix
+# Yasmin Yasmin
+# Rodrigo Lisboa Da Silva
+# *Lucas Gabriel Fernandes Johann
---
+++ CristianFernandes/CTD/Front-End2/Aula7/Desafio/script.js
@@ -0,0 +1,8 @@
+ function trocar() {
+    var value = prompt("Escolha a imagem que deseja mudar! Sendo: 0 - Tigre, 1 - Leão, 2 - Guepardo, 3 - Pantera Negra, 4 - Jaguar, 5 - Guepardo")
+    let insertSRC = prompt("insira a URL da imagem")
+    let listaFelinos = document.querySelectorAll(".item");
+    let src = listaFelinos[value];
+    let img = src.querySelector('img')
+    img.setAttribute("src", `${insertSRC}`)
+} 
