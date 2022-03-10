314d402e1f16333d187600fdf38c549188e371a5
Cristian Fernandes <cristian.daniel.fernandes@gmail.com>
2022-03-10 13:11:32 -0300

    Teste de Readme

--- CristianFernandes/CTD/Front-End2/Aula7/Desafio/script.js
+++ CristianFernandes/CTD/Front-End2/Aula7/Desafio/script.js
@@ -1,4 +1,4 @@
- function trocar() {
+function trocar() {
     var value = prompt("Escolha a imagem que deseja mudar! Sendo: 0 - Tigre, 1 - Leão, 2 - Guepardo, 3 - Pantera Negra, 4 - Jaguar, 5 - Guepardo")
     let insertSRC = prompt("insira a URL da imagem")
     let listaFelinos = document.querySelectorAll(".item");
