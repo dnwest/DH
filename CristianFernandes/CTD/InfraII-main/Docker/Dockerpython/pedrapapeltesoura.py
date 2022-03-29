419eca1030f408567f5f3d7df4d1d3b696757b03
dnwest <cristian.daniel.fernandes@gmail.com>
2022-03-28 22:50:30 -0300

    Aula Infra Pyton

--- CristianFernandes/CTD/InfraII-main/Docker/Dockerpython/pedrapapeltesoura.py
+++ CristianFernandes/CTD/InfraII-main/Docker/Dockerpython/pedrapapeltesoura.py
@@ -2,12 +2,14 @@
 import random
 
 while True: 
-    aleatorio = random.randrange(0, 3)
+    aleatorio = random.randrange(0, 4)
     escolhaPc = ""
     print("1)Pedra")
     print("2)Papel")
     print("3)Tesoura")
-    print("4)Sair do Programa")
+    print("4)Spock")
+    print("5)Lagarto")
+    print("6)Sair do Programa")
     opcao = int(input("O que você escolhe: "))
     
     if opcao == 1:
@@ -17,6 +19,10 @@ while True:
     elif opcao == 3:
         escolhaUsuario = "tesoura"
     elif opcao == 4:
+        escolhaUsuario = "Spock"
+    elif opcao == 5:
+        escolhaUsuario = "Lagarto"
+    elif opcao == 6:
         print ("Nos vemos!")
         break
     else:
@@ -30,6 +36,10 @@ while True:
         escolhaPc = "papel"
     elif aleatorio == 2:
         escolhaPc = "tesoura"
+    elif aleatorio == 3:
+        escolhaPc = "Spock"
+    elif aleatorio == 4:
+        escolhaPc = "Lagarto"        
     print("PC escolheu: ", escolhaPc)
     print("...")
     
@@ -39,13 +49,21 @@ while True:
         print("Ganhou, tesoura corta papel")
     elif escolhaPc == "tesoura" and escolhaUsuario == "pedra":
         print("Ganhou, pedra amassa tesoura")
-        
+    elif escolhaPc == "Spock" and escolhaUsuario == "tesoura":
+        print("Ganhou, Spock quebra tesoura")    
+    elif escolhaPc == "lagarto" and escolhaUsuario == "Spock":
+        print("Ganhou, lagarto envenena Spock")
+
     if escolhaUsuario == "pedra" and escolhaPc == "papel":
         print("Perdeu, papel cobre pedra")
     elif escolhaUsuario == "papel" and escolhaPc == "tesoura":
         print("Perdeu, tesoura corta papel")
     elif escolhaUsuario == "tesoura" and escolhaPc == "pedra":
         print("Perdeu, pedra amassa tesoura")
+    elif escolhaUsuario == "lagarto" and escolhaPc == "pedra":
+        print("Perdeu, pedra esmaga lagarto")
+    elif escolhaUsuario == "Spock" and escolhaPc == "pedra":
+        print("Perdeu, Spock vaporiza pedra")        
     elif escolhaPc == escolhaUsuario:
         print("Empate")
     again = input("Jogar novamente? s/n: ")
