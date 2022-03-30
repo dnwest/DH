413e358c4b87e452f2a7d98328246b73ff78c8c2
dnwest <cristian.daniel.fernandes@gmail.com>
2022-03-29 23:01:28 -0300

    Aula16 Front e Infra Pyton

--- CristianFernandes/CTD/InfraII-main/Docker/Dockerpython/pedrapapeltesoura.py
+++ CristianFernandes/CTD/InfraII-main/Docker/Dockerpython/pedrapapeltesoura.py
@@ -1,5 +1,16 @@
 #encoding:UTF-8
 import random
+userScore = 0
+pcScore = 0
+totalScore = 0
+
+def percent():
+        if totalScore > 0: 
+            x = ((totalScore - pcScore) / totalScore) * 100
+            return x
+        elif totalScore == 0:
+            x = 0
+            return x
 
 while True: 
     aleatorio = random.randrange(0, 4)
@@ -9,7 +20,8 @@ while True:
     print("3)Tesoura")
     print("4)Spock")
     print("5)Lagarto")
-    print("6)Sair do Programa")
+    print("6) Show Scores")
+    print("7)Sair do Programa")
     opcao = int(input("O que você escolhe: "))
     
     if opcao == 1:
@@ -23,6 +35,11 @@ while True:
     elif opcao == 5:
         escolhaUsuario = "Lagarto"
     elif opcao == 6:
+        print ("Pontuações: ")
+        print ("Usuário: ", userScore)
+        print ("Pc: ", pcScore)
+        print ("Porcentagem de vitórias: ", percent(), "% " )
+    elif opcao == 7:
         print ("Nos vemos!")
         break
     else:
@@ -42,28 +59,39 @@ while True:
         escolhaPc = "Lagarto"        
     print("PC escolheu: ", escolhaPc)
     print("...")
-    
+
     if escolhaPc == "pedra" and escolhaUsuario == "papel":
         print("Ganhou, papel cobre pedra")
+        pcWin = 1
     elif escolhaPc == "papel" and escolhaUsuario == "tesoura":
         print("Ganhou, tesoura corta papel")
+        pcWin = 1
     elif escolhaPc == "tesoura" and escolhaUsuario == "pedra":
         print("Ganhou, pedra amassa tesoura")
+        pcWin = 1
     elif escolhaPc == "Spock" and escolhaUsuario == "tesoura":
-        print("Ganhou, Spock quebra tesoura")    
+        print("Ganhou, Spock quebra tesoura")
+        pcWin = 1   
     elif escolhaPc == "lagarto" and escolhaUsuario == "Spock":
         print("Ganhou, lagarto envenena Spock")
+        pcWin = 1 
+    
 
     if escolhaUsuario == "pedra" and escolhaPc == "papel":
         print("Perdeu, papel cobre pedra")
+        userWin = 1
     elif escolhaUsuario == "papel" and escolhaPc == "tesoura":
         print("Perdeu, tesoura corta papel")
+        userWin = 1
     elif escolhaUsuario == "tesoura" and escolhaPc == "pedra":
         print("Perdeu, pedra amassa tesoura")
+        userWin = 1
     elif escolhaUsuario == "lagarto" and escolhaPc == "pedra":
         print("Perdeu, pedra esmaga lagarto")
+        userWin = 1
     elif escolhaUsuario == "Spock" and escolhaPc == "pedra":
-        print("Perdeu, Spock vaporiza pedra")        
+        print("Perdeu, Spock vaporiza pedra")
+        userWin = 1   
     elif escolhaPc == escolhaUsuario:
         print("Empate")
     again = input("Jogar novamente? s/n: ")
@@ -74,3 +102,12 @@ while True:
         break
     else:
         print("Valor Invalido")
+
+        
+
+userScore = userScore + userWin
+pcScore = pcScore + pcWin
+totalScore = userScore + pcScore
+
+
+ 
